e2ba1037354e7cfa997c6f2093c4baac78910e70
David <davidmaringomez81@gmail.com>
2022-10-12 11:57:08 +0200

    Primera Subida

---
+++ src/pruebaconnetbeans/PruebaConNetbeans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebaconnetbeans;
+
+/**
+ *
+ * @author David Marín
+ */
+public class PruebaConNetbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
